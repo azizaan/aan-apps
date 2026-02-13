8fa9ef6e2498cbfc8fe6fd3c1aabe111bae780e7
azizaan <109931536+azizaan@users.noreply.github.com>
2026-02-13 08:48:00 +0700

    changes to datatables

--- resources/views/User.blade.php
+++ resources/views/User.blade.php
@@ -3,44 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Halaman User</title>
+    
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.0/css/dataTables.dataTables.min.css">
+    
     <style>
-        table {
-            border-collapse: collapse;
-            width: 80%;
-            margin: 20px 0;
-        }
-        table th, table td {
-            border: 1px solid #ddd;
-            padding: 12px;
-            text-align: left;
-        }
-        table th {
-            background-color: #4CAF50;
-            color: white;
-        }
-        table tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
+        body { font-family: sans-serif; padding: 20px; }
+        /* CSS styling manual Anda bisa tetap ada, tapi DataTables punya styling sendiri */
+        table { width: 100% !important; }
     </style>
 </head>
 <body>
     <h1>Halaman User</h1>
 
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>Email</th>
-        </tr>
-        @foreach($users as $user)
-        <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-        </tr>
-        @endforeach
+    <table id="myTable" class="display">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+            @endforeach
+        </tbody>
     </table>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
+
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable({
+                // Opsional: Custom bahasa Indonesia
+                language: {
+                    url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/id.json'
+                }
+            });
+        });
+    </script>
 </body>
 </html>
